00185688e6482e9365ca9c1083eaade620b595df
Sophia Anastasia Wellman <112637823+sophiastasia@users.noreply.github.com>
2023-02-26 15:13:19 +0100

    Added exercise 1

---
+++ oppgave1a.py
@@ -0,0 +1,13 @@
+def isFibonacciNumber(n):
+    '''
+    n: int
+    '''
+    a = 0
+    b = 1
+    while b<n:
+        temp = b
+        b += a
+        a = temp
+    return b == n
+
+print(isFibonacciNumber(5))
